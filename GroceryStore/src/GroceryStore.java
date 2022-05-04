63b607a09fd9968596e1084689c895cb5f6c8186
Aslıhan Şimşek <simseekasli@gmail.com>
2022-05-04 13:54:47 +0300

    - Starting Grocery Store Programming

--- GroceryStore/src/GroceryStore.java
+++ GroceryStore/src/GroceryStore.java
@@ -2,6 +2,6 @@ import javafx.scene.shape.SVGPath;
 
 public class GroceryStore {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Grocery Store Programing");
     }
 }
